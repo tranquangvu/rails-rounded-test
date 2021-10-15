38442e753a50826e01bf0d7802546a82a6f38bd6
tranquangvu <vutran.513@gmail.com>
2021-10-15 14:32:29 +0700

    Add annotate gem

--- backend/app/models/account.rb
+++ backend/app/models/account.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id          :integer          not null, primary key
+#  balance     :decimal(8, 2)
+#  bank_number :string
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Account < ApplicationRecord
   # constants
   INITIAL_BALANCE = 1000.freeze
--- backend/app/models/expense.rb
+++ backend/app/models/expense.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id          :integer          not null, primary key
+#  amount      :integer
+#  date        :date
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  account_id  :integer
+#
+# Indexes
+#
+#  index_expenses_on_account_id  (account_id)
+#
+# Foreign Keys
+#
+#  account_id  (account_id => accounts.id)
+#
 class Expense < ApplicationRecord
   # accociations
   belongs_to :account
